f8ea1129e7a81dfc5cb1efa507a5a28f181d7f30
Cracker02 <sbajracharya02@gmail.com>
2022-11-26 21:51:41 +0100

    first commit

---
+++ magik.py
@@ -0,0 +1,13 @@
+import pyautogui;
+
+time = 1
+
+for i in range(3 * time):
+    pyautogui.moveTo(100, 100, 5)
+    pyautogui.moveTo(200, 100, 5)
+    pyautogui.moveTo(200, 200, 5)
+    pyautogui.moveTo(100, 200, 5)
+
+
+
+    
